eb0193f6feae66117deb0262eb5fd9521baf1c5b
mayur-init1 <mayurcosmos007@gmail.com>
2022-05-26 14:25:19 +0530

    autoclosebrackets fixed

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,6 +4,7 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material-darker.css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
 
 function Editor() {
   
@@ -12,6 +13,7 @@ function Editor() {
       CodeMirror.fromTextArea(document.getElementById('editor'),{
         mode: {name: 'javascript', json: true},
         theme:'material-darker',
+        lint: true,
         autoCloseTags: true,
         autoCloseBrackets: true,
         lineNumbers: true,
