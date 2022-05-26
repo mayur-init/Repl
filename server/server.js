bea2a840102030376336426e33ba0fb10be196be
mayur-init1 <mayurcosmos007@gmail.com>
2022-05-26 15:07:40 +0530

    socket initialized

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) =>{
+    console.log(`socket connected, socket Id: ${socket.id}`);
+})
 
 app.get('/', (req, res)=>{
     res.send(`<h3>Express server is running...<h3>`);
 })
 
-app.listen(PORT, () => {console.log(`listening to port ${PORT}...`)});
+server.listen(PORT, () => {console.log(`listening on port ${PORT}...`)});
