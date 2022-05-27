7f2851f1f894f4a363e77b86da5b3a13429a2a21
mayur-init1 <mayurcosmos007@gmail.com>
2022-05-27 18:01:11 +0530

    Hompage overflow fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Homepage() {
   return (
     <div className='bg-zinc-900 h-screen min-w-max'>
-      <div className='flex flex-row justify-between'>
+      <div className='flex flex-row justify-between h-[10vh]'>
         <h1 className='text-2xl text-zinc-300 p-4'>Code<span className='text-zinc-500'>Sync</span></h1>
         <Link to='/room'><button className='btn btn-primary m-4'>Rooms</button></Link>
       </div>
-      <div className='grid place-content-center h-full'>
+      <div className='grid place-content-center h-[90vh]'>
         <p className='text-2xl text-zinc-600 justify-center px-4 mx-4'>Currently homepage is not avalible, It will be avalible soon.</p>
       </div>
     </div>
