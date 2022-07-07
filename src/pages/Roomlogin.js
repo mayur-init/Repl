59b3182b4cf0bb1fce658de4e028411e8b09fb8d
mayur-init1 <mayurcosmos007@gmail.com>
2022-07-07 15:12:19 +0530

    ui fix1

--- src/pages/Roomlogin.js
+++ src/pages/Roomlogin.js
@@ -40,10 +40,11 @@ function Roomlogin() {
   }
 
   return (
-    <div className='bg-gray-200 dark:bg-zinc-900 h-screen grid place-items-center min-w-max p-4'>
-      <div className='item-center bg-gradient-to-r from-pink-600 to-purple-600 rounded-xl w-[45vh] md:w-[52vh] h-[38vh] -inset-3 blur opacity-20 hover:opacity-60 hover:transition duration-1000 hover:duration-800 animate-pulse'></div>
-        <div className='absolute container bg-gray-300 dark:bg-zinc-800 w-1/4 p-7 flex flex-col shadow-2xl dark:shadow-none rounded-xl min-w-max '>
-          <h1 className='text-2xl text-gray-600 dark:text-zinc-400 mb-6'>Code<span className='dark:text-zinc-500 px-1'>Sync</span></h1>
+    <div className='flex flex-col justify-center bg-gray-200 dark:bg-zinc-900 h-screen min-w-max'>
+      <div className='bg-gray-300 dark:bg-zinc-800 h-[70vh] grid place-items-center min-w-max'>
+        <img src='./Images/learning.svg' alt='learning' className='justify-self-start m-6 h-[70vh] w-[70vh]' />
+        <div className='absolute container bg-gray-300 dark:bg-zinc-900 w-1/4 p-7 flex flex-col shadow-2xl dark:shadow-none rounded-xl min-w-max'>
+          <h1 className='text-2xl text-zinc-600 dark:text-zinc-400 mb-6'>CodeSync</h1>
 
           <h4 className='text-gray-600 dark:text-zinc-400 py-2'>Paste your room Id</h4>
           <input type='text' onChange={(e) => { setRoomId(e.target.value) }} value={roomId} onKeyUp={handleInputEnter} placeholder='roomId' className='px-3 py-1 mb-6 text-gray-700 bg-zinc-200 rounded-md'></input>
@@ -57,7 +58,9 @@ function Roomlogin() {
             <button onClick={createNewRoom} className='text-gray-600 dark:text-zinc-400 hover:text-zinc-800  pt-2 pl-1'>new room</button>
           </span>
         </div>
+      </div>
     </div>
+
   )
 }
 
