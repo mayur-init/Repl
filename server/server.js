5a080588d2e55655d89f41b2ecd0aecbc222bd9d
mayur-init1 <mayurcosmos007@gmail.com>
2022-08-02 21:58:36 +0530

    server.js fix

--- server/server.js
+++ server/server.js
@@ -116,15 +116,15 @@ io.on('connection', (socket) =>{
 })
 
 // -------------static assets for deployment ----------------------
-// __dirname = path.resolve();
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname, '/frontend/build')));
-//     app.get('*', (req, res) =>{
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     });
-// }else{
-//  router.get('/', (req, res) =>{res.send('<h2>Express server is running ...</h2>')});
-// }
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}else{
+ router.get('/', (req, res) =>{res.send('<h2>Express server is running ...</h2>')});
+}
 //---------------------------------------------------------------------
 
 router.post('/compile', codeRunController.codeRun);
