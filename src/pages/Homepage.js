9b0fd107434fb4eb9522bf5a55ec97a34958e327
mayur-init1 <mayurcosmos007@gmail.com>
2022-08-14 20:14:27 +0530

    homepage text fix

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -20,7 +20,7 @@ function Homepage() {
         <div className='flex'>
           {/* <img src='./Images/team.svg' alt='team' className='m-4 h-[45.4vh] w-[55vh]' /> */}
           <div className='flex flex-col justify-center h-auto items-center p-2 shrink'>
-            <p className='md:text-3xl text-xl break-normal hidden md:contents text-zinc-500 m-2'>CodeSync is a realtime code colaboration tool for learners.</p>
+            <p className='md:text-3xl text-xl break-normal hidden md:contents text-zinc-500 m-2'>CodeSync is a realtime code collaboration tool for learners.</p>
             <p className='flex justify-center md:text-4xl text-2xl break-words text-zinc-500 m-2'>Haven't used CodeSync yet?</p>
             <p className='flex justify-center md:text-2xl text-xl text-zinc-500 m-2'>Create your first room now.</p>
             <Link to='/room' className='flex justify-center m-4'><button className='btn btn-primary border-2 m-4 animate-bounce'>Rooms</button></Link>
